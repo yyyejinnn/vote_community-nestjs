d031d44061a6cee483627b083d6ac1e69170f170
ian <lyjin2007@gmail.com>
2022-12-13 18:30:56 +0900

    feat: Add sqlite package

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -2,7 +2,7 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeORMConfig: TypeOrmModuleOptions = {
   type: 'sqlite',
-  database: 'vote-app',
+  database: 'vote-database',
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: true,
 };
