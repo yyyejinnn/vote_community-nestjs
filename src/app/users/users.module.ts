3bd7dcdaf3c3cf2721dcf696210e25ece0eb779b
yejin <lyjin2007@gmail.com>
2023-03-05 03:51:57 +0900

    feat: Add updateProfilePhoto service

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -2,6 +2,8 @@ import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RefreshTokensEntity, UsersEntity } from '@vote/common';
 import { TokenService } from '../auth/auth.service';
+import { S3Service } from '../service/s3.service';
+import { ServiceModule } from '../service/service.module';
 import { VotesModule } from '../votes/votes.module';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
@@ -10,9 +12,10 @@ import { UsersService } from './users.service';
   imports: [
     forwardRef(() => VotesModule),
     TypeOrmModule.forFeature([UsersEntity, RefreshTokensEntity]),
+    ServiceModule,
   ],
   controllers: [UsersController],
-  providers: [UsersService, TokenService],
+  providers: [UsersService, TokenService, S3Service],
   exports: [TypeOrmModule, UsersService, TokenService],
 })
-export class UsersModule {}
+export class UsersModule { }
